2222aeedbeb84cd4b7d2bccb5dd860b3146fe219
Janaina <janaina.reis@fatec.sp.gov.br>
2024-06-07 15:31:18 -0300

    palavra dedicada final

---
+++ tipos-variaveis/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        
+        final double NUM_PI = 3.14; 
+        /*  final e o nome da variável em caixa alta 
+        significa que o valor da variável não pode ser mudado */
+        System.out.println(NUM_PI);
+    }
+}
