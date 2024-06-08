4e949bfa4d30c827a7152a6a09a522d625c5a976
Janaina <janaina.reis@fatec.sp.gov.br>
2024-06-08 10:48:56 -0300

    aprendendo os tipos

---
+++ operadores/src/Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        double soma = 10.5 + 15.7;
+        int modulo = 18 % 4; // resto da divisão
+        System.out.println(soma);
+        System.out.print(modulo);
+    }
+}
+/* Operador de atribuição '=' símbolo de igual 
+ * Operador aritméticos: '+ - / *' adição, subtração, multiplicação e divisão
+ * Operador lógico:
+ * Operador unário: aplicado junto com operador aritmético (incrementar, decrementar)
+*/
