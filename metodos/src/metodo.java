ab39f05c023f46f5c0a1bc9b48252fe1e1c576ed
Janaina <janaina.reis@fatec.sp.gov.br>
2024-06-09 16:58:51 -0300

    atualizar nome

--- metodos/src/metodo.java
+++ metodos/src/metodo.java
@@ -1,5 +1,11 @@
-public class App {
+public class Metodo {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        //int n1, n2;
+        //somar(int n1, int n2){}
+       // concluirProcessamento(){}
+
     }
 }
+
+/*Deve ser nomeado como verbo, seguir o padrão camelCase */
