f115ffc2d1928ade0ad50070f6ff50796ef4bc3b
Janaina <janaina.reis@fatec.sp.gov.br>
2024-06-09 21:06:42 -0300

    Palavras reservadas

---
+++ palavra-reservada/src/PalavraReservada.java
@@ -0,0 +1,10 @@
+public class PalavraReservada {
+/*import: importa biblioteca
+ * package: qual pacote de arquivo uma classe pertence
+ * Modificadores de acesso: publi, private e protected
+ * Tipos primitivos: int, long, void, char, sort, float, double, boolean, byte
+ * class, extends, abstract, final, implements, interface
+ * new: instancia um objeto
+ * static: faz um método ou uma variável pertencer a classe sem precisaar ser instanciado
+ */   
+}
