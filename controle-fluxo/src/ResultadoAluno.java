6248b4feb10ba141df339d886a3db5dcc4021722
Janaina <janaina.reis@fatec.sp.gov.br>
2024-06-12 14:18:46 -0300

    else if, condição ternária e switch case

--- controle-fluxo/src/ResultadoAluno.java
+++ controle-fluxo/src/ResultadoAluno.java
@@ -9,9 +9,12 @@ public class ResultadoAluno {
         } else {
             System.out.println("Reprovado");
         }
+        // Condição ternária
+        String  resultado = nota>=7 ? "Aprovado" : nota>=5 && nota <7 ? "Recuperação" : "Reprovado";
+        System.out.print(resultado); // else if usando estrutura ternária
     }
 }
 /*Usando condição ternária
-String  resultado = nota>=7 ? "Aprovado" : "Reprovado"
+String  resultado = nota>=7 ? "Aprovado" : nota>=5 && nota <7 ? "Recuperação" : "Reprovado";
 System.out.print(resultado);
 */ 
