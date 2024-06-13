3a45496386ff3efb6a1436af5d4858661702ec19
Janaina <janaina.reis@fatec.sp.gov.br>
2024-06-13 13:02:39 -0300

    try catch

--- controle-fluxo/src/TratamentoExc.java
+++ controle-fluxo/src/TratamentoExc.java
@@ -1,8 +1,10 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class TratamentoExc {
     public static void main(String[] args) {
+        try {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Digite seu nome");
@@ -18,8 +20,13 @@ public class TratamentoExc {
         double altura = scanner.nextDouble();
 
         // Imprimindo os dados obtidos pelo usuário
-        System.out.println("Olá, me chamo " + nome + " " + sobrenome);
+        System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos ");
-        System.out.println("Minha altura é " + altura + " cm");  
+        System.out.println("Minha altura é " + altura + " cm");
+        scanner.close();
+        } 
+        catch (InputMismatchException e)   {
+            System.out.println("Os campos idade e altura precisam ser numericos");
+        }
     }
 }
