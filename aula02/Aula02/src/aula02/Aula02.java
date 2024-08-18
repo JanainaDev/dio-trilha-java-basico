3dd0b71e80e6c862dc6e8b9915a2633115fce9ed
Janaina <168225017+JanainaDev@users.noreply.github.com>
2024-08-17 22:30:41 -0300

    Update Aula02.java

--- aula02/Aula02/src/aula02/Aula02.java
+++ aula02/Aula02/src/aula02/Aula02.java
@@ -2,7 +2,7 @@ package aula02;
 public class Aula02
  {
     public static void main(String[] args) {
-        Caneta c1 = new Caneta();
+        Caneta c1 = new Caneta(); //instanciando um objeto
         c1.modelo = "Bic Crystal";
         c1.cor = "Azul";
         c1.ponta = 0.5f;
